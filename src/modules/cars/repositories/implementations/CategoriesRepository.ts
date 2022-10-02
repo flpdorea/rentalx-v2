76b2a0b00f8920172476fb0da2c3acfdcfeb9a9f
flpdorea <felipeldorea@gmail.com>
2022-10-02 19:02:00 -0300

    removing useless code

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -7,18 +7,9 @@ class CategoriesRepository implements ICategoriesRepository{
 
   private repository: Repository<Category>
 
-  private static INSTANCE: CategoriesRepository
-
   constructor() {
     this.repository = AppDataSource.getRepository(Category)
   }
-
-  // public static getInstance(): CategoriesRepository {
-  //   if (!CategoriesRepository.INSTANCE) {
-  //     CategoriesRepository.INSTANCE = new CategoriesRepository()
-  //   }
-  //   return CategoriesRepository.INSTANCE
-  // }
 
   async create({name, description}: ICreateCategoryDTO): Promise<void> {
     const category = this.repository.create({name, description})
