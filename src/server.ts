bead1bd17741b91f9c2b44c044f7d7cb3f235699
flpdorea <felipeldorea@gmail.com>
2022-10-05 18:16:20 -0300

    adding documentation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import { AppDataSource } from './data-source'
 import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
 import './shared/container'
+import swaggerFile from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(3000, () => console.log('Server is running!'))
 
