1ba5247c6466d48044fbee272fc3422da1c41e9a
flpdorea <felipeldorea@gmail.com>
2022-10-22 03:11:10 -0300

    adding cars migration

--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -5,10 +5,6 @@ import { User } from '../../../modules/accounts/infra/typeorm/entities/User'
 import { Category } from '../../../modules/cars/infra/typeorm/entities/Category'
 import { Specification } from '../../../modules/cars/infra/typeorm/entities/Specification'
 
-import { CreateCategories1664740329592 } from './migration/1664740329592-CreateCategories'
-import { CreateSpecifications1664908935866 } from './migration/1664908935866-CreateSpecifications'
-import { CreateUsers1665010616433 } from './migration/1665010616433-CreateUsers'
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'database',
@@ -19,10 +15,6 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: false,
   entities: [Category, Specification, User],
-  migrations: [
-    CreateCategories1664740329592, 
-    CreateSpecifications1664908935866,
-    CreateUsers1665010616433
-  ],
+  migrations: ['./migration/*.ts'],
   subscribers: [],
 })
